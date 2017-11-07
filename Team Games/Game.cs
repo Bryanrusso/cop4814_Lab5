de0f95b9421c3d184883d25005b8d373844bfffe
Bryanrusso <bryanrussowsky@gmail.com>
2017-11-07 17:24:30 -0500

    Update Game.cs

--- Team Games/Game.cs
+++ Team Games/Game.cs
@@ -11,7 +11,7 @@ namespace TeamGames
     {
         public string Team1 { get; set; }
         public string Team2 { get; set; }
-        public int Team1Score { get; set; }
+        public int YX { get; set; }
         public int Team2Score { get; set; }
 
         public Game() {  }
